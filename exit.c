0dd98f1a773343eee687d9837a490b8160bf5ea4
Asuma9 <paulasuma10@gmail.com>
2023-10-19 16:55:56 +0300

    Simple shell

--- exit.c
+++ exit.c
@@ -6,10 +6,10 @@
 */
 int shellby_exit(char **args)
 {
-    if (_strcmp(args[0], "exit") != 0)
-    {
-        return (-1);
-    }
+	if (_strcmp(args[0], "exit") != 0)
+	{
+		return (-1);
+	}
 	if (args[1] != NULL)
 	{
 		_eputs("Usage: exit\n");
